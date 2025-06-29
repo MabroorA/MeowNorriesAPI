6a65ed388edd301ce46602c97408e780cafccd0a
Mabroor Ahmed <109113298+MabroorA@users.noreply.github.com>
2025-06-30 00:02:50 +0100

    feat: implement case-insensitive replacement of 'Chuck Norris' with 'Meow Norris' in joke API

--- app/api/joke.py
+++ app/api/joke.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, HTTPException
 from app.config import CHUCK_NORRIS_API_URL
-
 from app.services.joke_service import fetch_chuck_norris_joke
+import re
 
 router = APIRouter()
 
@@ -10,7 +10,7 @@ async def get_joke():
     try:
         joke_data = await fetch_chuck_norris_joke()
         joke = joke_data["value"]
-        modified_joke = joke.replace("Chuck Norris", "Meow Norris")
+        modified_joke = re.sub(r"Chuck Norris", "Meow Norris", joke, flags=re.IGNORECASE)
         return {"joke": modified_joke}
     except Exception:
         raise HTTPException(status_code=503, detail="Failed to fetch joke from external API")
